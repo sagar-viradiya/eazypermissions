279b7250c65afd6a3231f3e1647534ab0118548d
Sagar Viradiya <sagarviradiya4444@gmail.com>
2019-04-22 12:04:53 +0530

    PermissionManager implementation
    - Added logic to handle run time permission in headless fragment.

--- livedatapermission/src/main/java/com/example/livedatapermission/PermissionManager.kt
+++ livedatapermission/src/main/java/com/example/livedatapermission/PermissionManager.kt
@@ -1,31 +1,69 @@
 package com.example.livedatapermission
 
-
-import android.os.Bundle
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.content.pm.PackageManager
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BlankFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
  */
-class BlankFragment : Fragment() {
+class PermissionManager : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    private val permissionResultLiveData: MutableLiveData<PermissionResult> by lazy {
+        MutableLiveData<PermissionResult>()
     }
 
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            permissionResultLiveData.postValue(PermissionResult.PermissionGranted(requestCode))
+        } else if (shouldShowRequestPermissionRationale(permissions[0])) {
+            permissionResultLiveData.postValue(PermissionResult.PermissionDenied(requestCode))
+        } else {
+            permissionResultLiveData.postValue(PermissionResult.PermissionDeniedPermanently(requestCode))
+        }
+    }
+
+    private fun requestPermissions(requestId: Int, vararg permissions: String) {
+        val notGranted = permissions.filter {
+            ContextCompat.checkSelfPermission(requireActivity(), it) != PackageManager.PERMISSION_GRANTED
+        }.toTypedArray()
+
+        when {
+            notGranted.isEmpty() -> permissionResultLiveData.postValue(PermissionResult.PermissionGranted(requestId))
+            shouldShowRequestPermissionRationale(notGranted[0]) ->
+                permissionResultLiveData.postValue(PermissionResult.ShowRational(requestId))
+            else -> {
+                requestPermissions(notGranted, requestId)
+            }
+        }
+
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @return A new instance of fragment BlankFragment.
-         */
+
+        private const val TAG = "PermissionManager"
+
         @JvmStatic
-        fun newInstance() = BlankFragment()
+        fun requestPermissions(activity: AppCompatActivity, requestId: Int, vararg permissions: String) {
+            if (activity.supportFragmentManager.findFragmentByTag(TAG) != null) {
+                (activity.supportFragmentManager.findFragmentByTag(TAG) as PermissionManager).requestPermissions(requestId, *permissions)
+            } else {
+                if (activity !is PermissionObserver) {
+                    throw IllegalArgumentException("Activity/Fragment must implement PermissionObserver")
+                } else {
+                    val permissionManager = PermissionManager()
+                    activity.supportFragmentManager.beginTransaction().add(permissionManager, TAG).commit()
+                    activity.setupObserver(permissionManager.permissionResultLiveData)
+                }
+            }
+        }
+    }
+
+    interface PermissionObserver {
+        fun setupObserver(permissionResultLiveData: LiveData<PermissionResult>)
     }
 }
