2248b372acd69e150d396e517ab3e44b2cd912a8
Sagar Viradiya <sagarviradiya4444@gmail.com>
2019-06-02 17:24:36 +0530

    Initial UI setup for sample app
    - Added 2 fragments one for coroutines and one for livedata.
    - Wired up 2 fragments using navigation components.
    - Added dependencies for navigation and design support libs.
    - Refactored MainActivity to hook navigation drawer with navigation graph.
    - Added navigation graph.
    - Added menu item for navigation drawer.

---
+++ sample/src/main/java/com/example/sample/CoroutinesFragment.kt
@@ -0,0 +1,25 @@
+package com.example.sample
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class CoroutinesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_coroutines, container, false)
+    }
+
+
+}
--- sample/src/main/java/com/example/sample/MainActivity.kt
+++ sample/src/main/java/com/example/sample/MainActivity.kt
@@ -1,78 +1,26 @@
 package com.example.sample
 
-import android.Manifest
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.app.AlertDialog
-import android.util.Log
-import android.widget.Toast
-import com.easypermissions.livedatapermission.PermissionManager
-import com.easypermissions.common.model.PermissionResult
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), PermissionManager.PermissionObserver {
-
-    companion object {
-        const val TAG = "MainActivity"
-    }
-
-    override fun setupObserver(permissionResultLiveData: LiveData<PermissionResult>) {
-        permissionResultLiveData.observe(this, Observer<PermissionResult> {
-            when (it) {
-                is PermissionResult.PermissionGranted -> {
-                    if (it.requestId == 1) {
-                        Log.i(TAG, "Permission granted")
-                        Toast.makeText(this@MainActivity, "Permission granted", Toast.LENGTH_SHORT).show()
-                    }
-                }
-                is PermissionResult.PermissionDenied -> {
-                    if (it.requestId == 1) {
-                        Log.i(TAG, "Permission denied")
-                        Toast.makeText(this@MainActivity, "Permission denied", Toast.LENGTH_SHORT).show()
-                    }
-                }
-                is PermissionResult.PermissionDeniedPermanently -> {
-                    if (it.requestId == 1) {
-                        Log.i(TAG, "Permission denied permanently")
-                        Toast.makeText(this@MainActivity, "Permission denied permanently", Toast.LENGTH_SHORT).show()
-                    }
-                }
-                is PermissionResult.ShowRational -> {
-                    if (it.requestId == 1) {
-                        val alertDialog = AlertDialog.Builder(this@MainActivity)
-                            .setMessage("We need permission")
-                            .setTitle("Rational")
-                            .setPositiveButton("OK") { _, _ ->
-                                PermissionManager.requestPermissions(
-                                    this@MainActivity,
-                                    1,
-                                    Manifest.permission.ACCESS_FINE_LOCATION,
-                                    Manifest.permission.READ_CONTACTS
-                                )
-                            }
-                            .setNegativeButton("Cancel") { dialog, _ ->
-                                dialog.dismiss()
-                            }.create()
-                        alertDialog.show()
-                        Log.i(TAG, "Show rational")
-                    }
-                }
-            }
-        })
-    }
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener {
-            PermissionManager.requestPermissions(
-                this@MainActivity,
-                1,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.READ_CONTACTS
-            )
-        }
+        val navController = findNavController(R.id.nav_host_frag)
+        setSupportActionBar(toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+        navigation_view.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.nav_host_frag), drawer_layout)
     }
 }
