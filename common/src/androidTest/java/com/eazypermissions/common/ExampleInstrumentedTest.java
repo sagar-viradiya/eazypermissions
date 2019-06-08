6a2777a8cbbca0b1b5538d3ee244c76746c61a68
Sagar Viradiya <sagarviradiya4444@gmail.com>
2019-06-08 18:48:44 +0530

    Updated package name
    - Manifest package name update.

--- common/src/androidTest/java/com/eazypermissions/common/ExampleInstrumentedTest.java
+++ common/src/androidTest/java/com/eazypermissions/common/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.easypermissions.common;
+package com.eazypermissions.common;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.easypermissions.common.test", appContext.getPackageName());
+        assertEquals("com.eazypermissions.common.test", appContext.getPackageName());
     }
 }
--- livedatapermission/src/androidTest/java/com/eazypermissions/livedatapermission/ExampleInstrumentedTest.java
+++ livedatapermission/src/androidTest/java/com/eazypermissions/livedatapermission/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.easypermissions.coroutinespermission;
+package com.eazypermissions.livedatapermission;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -16,11 +16,12 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.easypermissions.coroutinespermission.test", appContext.getPackageName());
+        assertEquals("com.example.livedatapermission.test", appContext.getPackageName());
     }
 }
